9a9f2f36ec7e6d029f545e3fdca2d76d71f0e5aa
kemasghani <kemasghani123@gmail.com>
2023-09-16 22:42:27 +0700

    add responsiveness

--- src/components/SiteFooter.jsx
+++ src/components/SiteFooter.jsx
@@ -33,33 +33,7 @@ export function SiteFooter() {
               </li>
             </ul>
           </div>
-          <div className="flex flex-row items-center gap-x-10 max-w-xs">
-            <div className="border border-neutral-900 rounded-lg">
-              <div className="relative flex h-24 w-24 flex-none items-center justify-center ">
-                <Icons.RoundedCorners className="absolute inset-0 h-full w-full stroke-gray-400" />
-                <Icons.QRCode className="bg-slate-200" />
-              </div>
-            </div>
-            <div>
-              <p className="font-semibold text-white">Download the app</p>
-              <p>Scan the QR code to download the app from the App Store.</p>
-            </div>
-          </div>
         </div>
-      </div>
-      <div className="flex flex-col-reverse md:flex-row items-center justify-between gap-4 border-t border-neutral-900 pt-8 pb-12 text-[15px] tracking-wide">
-        <p>© Copyright {new Date().getFullYear()}. All rights reserved.</p>
-        <p>
-          Built by{" "}
-          <a
-            href="https://github.com/edroamz"
-            target="_blank"
-            rel="noreferrer"
-            className="text-slate-300 hover:text-white underline underline-offset-4 decoration-slate-200"
-          >
-            Eduardo Ambriz
-          </a>
-        </p>
       </div>
     </footer>
   );
